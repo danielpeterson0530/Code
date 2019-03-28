5b4786363e1d5329e1dd5117555058f0c35d7f68
danielpeterson0530 <45879373+danielpeterson0530@users.noreply.github.com>
2019-03-28 09:46:05 -0500

    Create R_windowing_gc_plot_function.R

---
+++ R/R_windowing_gc_plot_function.R
@@ -0,0 +1,15 @@
+# R Function to create a simple plot for GC content with sliding window size
+
+plotwindowingGC <- function(windowsize, inputseq)
+{
+  gcwindow <- seq(1, length(inputseq)-windowsize, by = windowsize) 
+  n <- length(gcwindow)
+  chunks <- numeric(n)
+    for (i in 1:n) {
+      chunk <- inputseq[gcwindow[i]:(gcwindow[i]+(windowsize-1))]
+      chunkgc <-GC(chunk)
+      #print(chunkgc) #uncomment to print each chunk's GC to screen
+      chunks[i] <- chunkgc
+    }
+  plot(gcwindow,chunks,type="b",xlab="Nucleotide Start Position",ylab="GC Content", main=paste("GC Plot with windowsize:",windowsize))
+}

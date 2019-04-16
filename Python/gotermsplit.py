304caaafb7178193edadb05459dd672b071adb14
danielpeterson0530 <45879373+danielpeterson0530@users.noreply.github.com>
2019-04-16 14:13:04 -0500

    Update gotermsplit.py

--- Python/gotermsplit.py
+++ Python/gotermsplit.py
@@ -1,5 +1,5 @@
 #!/sbin/python3
-#split GOterm data and return as csv
+#split GOterm data
 
 # NOTE: pass file named as test.txt in following format:
 # GOterm,Genes
@@ -8,13 +8,18 @@
 # GO:003,AFLA_008|AFLA_009
 # GO:004,AFLA_099|AFLA_098
 # GO:005,AFLA_097
+#
+# NOTE: will drop empty cells (not spaces/tabs, just empty)
 
 import pandas as pd
+import numpy as np
 
 df1 = pd.read_csv("~/test/test.txt")
 df = pd.DataFrame(data=df1)
 new_df = pd.DataFrame(df.Genes.str.split('|').tolist(), index=df.GOterm).stack()
 new_df = new_df.reset_index([0, 'GOterm'])
 new_df.columns = ['GOterm', 'Genes']
+new_df.replace('', np.nan, inplace=True)
+new_df.dropna(inplace=True)
 csv = new_df.to_csv(index=False)
 print(csv)

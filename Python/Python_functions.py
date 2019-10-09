e3d002367e90434275e9227dd30d98f2c1462c4b
danielpeterson0530 <45879373+danielpeterson0530@users.noreply.github.com>
2019-10-09 13:01:08 -0500

    Update Python_functions.py
    Added some new functions

--- Python/Python_functions.py
+++ Python/Python_functions.py
@@ -10,7 +10,7 @@ def PatternCount(Text, Pattern):
             count = count+1
     return count
 
-#Script to calculate reverse complement of given Pattern
+#Function to calculate reverse complement of given Pattern
 ##Requires reverse and complement functions
 def ReverseComplement(Pattern):
     reverse = Reverse(Pattern)
@@ -38,3 +38,29 @@ def PatternMatching(Pattern, Genome):
             if Pattern == Genome[i:i+len(Pattern)]:
                 positions.append(i)
     return positions
+
+#Function to parse large fasta files into dictionary with label and sequence
+def parse_fasta(file):
+        parsed_fasta = {}                       
+        strings = file.strip().split('>')       
+
+        for line in strings:                   
+                if len(line) == 0:           
+                        continue
+                parts = line.split()          
+                label = parts[0]              
+                bases = ''.join(parts[1:])    
+                parsed_fasta[label] = bases     
+
+        return parsed_fasta
+
+#Function to determine %GC content of sequence data
+def gc_content(data):
+        total_len = len(data)                   
+        count = 0                               
+
+        for base in data:                       
+                if base == 'G' or base == 'C':  
+                        count += 1
+
+        return 100 * (float(count) / total_len) 

46cb93315ff1d4fb5bc13c6ef43d4718659cfffe
danielpeterson0530 <45879373+danielpeterson0530@users.noreply.github.com>
2019-10-18 09:39:41 -0500

    Update patternCount.py

--- Python/patternCount.py
+++ Python/patternCount.py
@@ -1,7 +1,23 @@
-#Function to count supplied Pattern in a reference Text
-def patternCount(text, pattern):
-    count = 0
-    for i in range(len(text)-len(pattern)+1):
-        if text[i:i+len(pattern)] == pattern:
-            count = count+1
-    return count
+#!/usr/bin/python3
+import sys
+
+def patternCount(pattern, dna):
+        positions = []
+        for i in range(len(dna) - len(pattern)+1):
+                if pattern == dna[i:i+len(pattern)]:
+                        positions.append(i+1)
+        print(*positions, sep = ' ')
+        return
+
+
+def main(arg):
+        data = open(arg).read().split()
+        dna = str(data[0])
+        pattern = str(data[1])
+        patternCount(pattern, dna)
+        return
+
+if len(sys.argv) > 1:
+        main(sys.argv[1])
+else:
+        print("improper usage: python3 tool.py ./file")

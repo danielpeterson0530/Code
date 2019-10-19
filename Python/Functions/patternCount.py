e880ac16d9c804fcdce1fe43ba84179e7c3b97dc
danielpeterson0530 <45879373+danielpeterson0530@users.noreply.github.com>
2019-10-19 14:36:37 -0500

    Create patternCount.py

---
+++ Python/Functions/patternCount.py
@@ -0,0 +1,7 @@
+#Python function to count number of occurences of given pattern
+def patternCount(text, pattern):
+    count = 0
+    for i in range(len(text)-len(pattern)+1):
+        if text[i:i+len(pattern)] == pattern:
+            count = count+1
+    return count

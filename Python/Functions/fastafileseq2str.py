52d4d7ce910b9142909211e1f600dcca783f62c4
danielpeterson0530 <45879373+danielpeterson0530@users.noreply.github.com>
2020-01-02 19:55:55 -0600

    Delete fastafileseq2str.py

--- Python/Functions/fastafileseq2str.py
+++
@@ -1,9 +0,0 @@
-def fastafileseq2str(filename):
-   strings = open(filename).read().strip().split('>')
-   for line in strings:
-      if len(line) == 0:
-         continue
-      parts = line.split()
-      label = parts[0]
-      bases_str = ''.join(parts[1:])
-   return bases_str

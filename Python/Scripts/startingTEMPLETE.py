e07ec647d56b36bc96c18a9e92a6a475656fc87d
danielpeterson0530 <45879373+danielpeterson0530@users.noreply.github.com>
2020-01-02 19:46:57 -0600

    Create startingTEMPLETE.py

---
+++ Python/Scripts/startingTEMPLETE.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# desc
+import sys
+
+
+def main(arg):
+
+   return
+
+
+## requires sys package
+if len(sys.argv) > 1:
+   main(sys.argv[1])
+else:
+   print("improper usage: python3 tool.py ./filename")

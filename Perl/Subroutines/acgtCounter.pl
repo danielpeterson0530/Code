f59903c4709b24d7ec10f3571e9fc9b481b59d07
danielpeterson0530 <45879373+danielpeterson0530@users.noreply.github.com>
2020-01-02 23:40:14 -0600

    Update acgtCounter.pl

--- Perl/Subroutines/acgtCounter.pl
+++ Perl/Subroutines/acgtCounter.pl
@@ -1,5 +1,5 @@
 #Perl subroutine to return the nucleotide counts for acgt for a DNA sequence string
-sub acgtCounter {;
+sub acgtCounter {
         $seq = shift @_;
         my ($a, $c, $g, $t) = ($seq =~ s/A/A/ig, $seq =~ s/C/C/ig, $seq =~ s/G/G/ig, $seq =~ s/T/T/ig);
         return ($a, $c, $g, $t);

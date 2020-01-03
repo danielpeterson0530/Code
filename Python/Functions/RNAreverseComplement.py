5c58c52d89be29a106731597d6d156d31b8741d9
danielpeterson0530 <45879373+danielpeterson0530@users.noreply.github.com>
2020-01-02 19:53:08 -0600

    Update RNAreverseComplement.py

--- Python/Functions/RNAreverseComplement.py
+++ Python/Functions/RNAreverseComplement.py
@@ -1,3 +1,4 @@
+#Python function to return reverse complement of RNA
 def RNAreverseComplement(seq):
    complement = {'A': 'U', 'C': 'G', 'G': 'C', 'U': 'A'}
    return "".join(complement.get(base, base) for base in reversed(seq))

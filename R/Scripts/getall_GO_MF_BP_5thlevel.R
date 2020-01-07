65aff065908e40084f71f1d7b25a43338f743ea8
danielpeterson0530 <45879373+danielpeterson0530@users.noreply.github.com>
2020-01-07 15:28:01 -0600

    Create getall_GO_MF_BP_5thlevel.R

---
+++ R/Scripts/getall_GO_MF_BP_5thlevel.R
@@ -0,0 +1,33 @@
+### R script to get all level 5 GO terms for Biological Process and Moloecular Function and write to file
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("GO.db")
+library(GO.db)
+
+
+getAllBPChildren <- function(goids)
+{
+  ans <- unique(unlist(mget(goids, GOBPCHILDREN), use.names=FALSE))
+  ans <- ans[!is.na(ans)] # Remove GO IDs that do not have any children
+}
+
+level1_BP_terms <- getAllBPChildren("GO:0008150")
+level2_BP_terms <- getAllBPChildren(level1_BP_terms)
+level3_BP_terms <- getAllBPChildren(level2_BP_terms)
+level4_BP_terms <- getAllBPChildren(level3_BP_terms)
+level5_BP_terms <- getAllBPChildren(level4_BP_terms)
+write(level5_BP_terms, file="all_fifthlevel_bp_goids.txt", sep="\n")
+
+getAllMFChildren <- function(goids)
+{
+  ans <- unique(unlist(mget(goids, GOMFCHILDREN), use.names=FALSE))
+  ans <- ans[!is.na(ans)]
+}
+
+level1_MF_terms <- getAllMFChildren("GO:0003674")
+level2_MF_terms <- getAllMFChildren(level1_MF_terms)
+level3_MF_terms <- getAllMFChildren(level2_MF_terms)
+level4_MF_terms <- getAllMFChildren(level3_MF_terms)
+level5_MF_terms <- getAllMFChildren(level4_MF_terms)
+write(level5_MF_terms, file="all_fifthlevel_mf_goids.txt", sep="\n")

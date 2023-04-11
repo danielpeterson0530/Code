306b41caae06f96f6123ff54a2e102f59df3e871
danielpeterson0530 <45879373+danielpeterson0530@users.noreply.github.com>
2023-04-10 21:16:43 -0500

    Update DP-PROJECT.py

--- Python/DP-PROJECT.py
+++ Python/DP-PROJECT.py
@@ -4,9 +4,10 @@ import subprocess
 from datetime import datetime
 
 def main():
-    device_number = "1"
+    devicenumber = "1"
     output = run_modules()
-    process_save_outputfile(output)
+    process_save_outputfile(output, devicenumber)
+    program_exit()
     
 def run_modules(): 
     def create_command(cmdstr):
@@ -21,21 +22,21 @@ def run_modules():
     def create_header(cmds):
         return "\n".join(("\nDate:\n  "+get_date(), "\nCommands run:", *["  "+c for c in cmds], "\nCommand Outputs:\n\n"))
     def run_module(cmdstr):
-        print("Running: " + cmd)
         cmd = create_command(cmdstr)
         output = run_subprocess(cmd)
-        processed_output = "COMMAND: \'" + cmdstr + "\'" + "\n" + "-"*20 + "START"+ "-"*20 + "\n" + output + "\n" + "-"*21 + "END"+ "-"*21, "\n\n"
+        processed_output = "COMMAND: \'" + cmdstr + "\'" + "\n" + "-"*20 + "START"+ "-"*20 + "\n" + output + "-"*21 + "END"+ "-"*21 + "\n\n\n"
         print(processed_output)
         return processed_output
     def prompt_ping():
-        response = prompt_only("Run an additional ping? (Type IPv4 address for Yes, N for No)\n")
+        response = prompt_only("Run an additional ping? (Type [ADDRESS] for Yes, 'n' for No)\n")
         if response == False:
-            return False
+            return False, False
         else:
             cmd = "ping -c4 " + response
+            print("\n\n")
             processed_output = run_module(cmd)
             return processed_output, cmd
-    cmds = ["nmcli device show", 
+    cmds = ["nmcli device show eth0", 
             "ping -c4 8.8.8.8",
             "ping -c4 www.yahoo.com",
             "traceroute -n www.yahoo.com",
@@ -44,31 +45,29 @@ def run_modules():
     concat_output = ""
     for cmd in cmds:
         processed_output = run_module(cmd)
-        concat_output = "\n".join((all_output, processed_output))
+        concat_output = "\n".join((concat_output, processed_output))
     while True:
         processed_output, cmd = prompt_ping()
         if processed_output == False:
             break
         else:
             cmds.append(cmd)
-            concat_output = "\n".join((all_output, processed_output))
+            concat_output = "\n".join((concat_output, processed_output))
     header = create_header(cmds)
     concat_output = header + "\n" + concat_output
     return concat_output
 
-def process_save_outputfile(output, device_number):
+def process_save_outputfile(output, devicenumber):
     def check_and_make_outdirs(devicenumber):
         userhomedir = get_user_home()
         dir1 = "".join((userhomedir, "/Documents"))
-        dir2 = "".join((dir1, "/test-", devicenumber, "customer-captures/"))
+        dir2 = "".join((dir1, "/Test", devicenumber, "-customer-captures/"))
         for directory in [dir1, dir2]:
             if not exists(directory):
                 os.makedirs(directory)
         return dir2
     def get_user_home():
         return os.path.expanduser('~')
-    def get_date():
-        return datetime.today().strftime("%Y-%m-%d")
     def get_full_path(file):
         return os.path.abspath(file)
     def exists(item):
@@ -79,26 +78,31 @@ def process_save_outputfile(output, device_number):
         f.close()
     def check_overwrite(outputfile_fullpath):
         if exists(outputfile_fullpath):
-            res = prompt_only("".join(("File: (", outputfile_fullpath, ") already exists. Overwrite? (Type any key for Yes, N for No)")))
+            res = prompt_only("".join(("File: (", outputfile_fullpath, ") already exists. Overwrite? (Type [ANY KEY] for Yes, 'n' for No)")))
             if res in ('n', 'N', 'no', 'NO', 'No'):
-                program_exit("File not written.")
+                program_exit("File not written. ")
             else:
                 return
-    def process_outputfilename(apnname, devicenumber):
+    def process_outputfilename(apnname, devicenumber, datacenter):
         outdir = check_and_make_outdirs(devicenumber)
         currentdate = get_date()
-        outputinfo = "".join((currentdate,"_APN-",apnname,"_Device-",devicenumber))
+        outputinfo = "".join((currentdate,"_", datacenter, "_APN-",apnname,"_Device-",devicenumber))
         outputfilename = "".join((outputinfo, "_TestResults.mcs",))
         outputfile_fullpath = "".join((outdir, outputfilename))  
         return outputfile_fullpath
-    apnname = prompt_only("\nDo you want to save output to file? (Type APN (numbers-only) if Yes, N if no)\n")
+    apnname = prompt_only("\nDo you want to save output to file? (Type APN (numbers-only) for Yes, 'n' for no)\n")
     if apnname == False:
-        program_exit("File not written.")
-    outputfile_fullpath = process_outputfilename(apnname, devicenumber)
+        program_exit("File not written. ")
+    datacenter = prompt_only("\nEnter data-center code:\n")
+    output = "Data center:\n  " + datacenter + "\n" + output
+    outputfile_fullpath = process_outputfilename(apnname, devicenumber, datacenter)
     check_overwrite(outputfile_fullpath)
     write_file(outputfile_fullpath, output)
     print("Output written to: " + outputfile_fullpath)
     return
+
+def get_date():
+    return datetime.today().strftime("%Y-%m-%d")
 
 def prompt_only(prompt_str):
     response = input(prompt_str)
@@ -108,6 +112,6 @@ def prompt_only(prompt_str):
     else:
         return response
 
-def program_exit(exit_string = "):
-    print(exit_string + " Program complete.")
+def program_exit(exit_string = ""):
+    print(exit_string + "Program complete.")
     quit()
